d85595e6d373a00863f48b4708fdf1d733ea8eb9
wuxing <2329697501@qq.com>
2014-09-18 13:42:00 +0800

    添加腾讯游戏挂机系统

---
+++ HangUp/login.php
@@ -0,0 +1,5 @@
+<?php
+define("DOMAIN","s2.app1101514477.qqopenapp.com");
+header("Location:".'http://'.DOMAIN.'/index.php?'.'adminisornot=1&seqid=0c182b4483356f3be02916fc1003f11a'
+		.'&openid='.$_GET['openid'].'&openkey=E3BFBE9CD664B4D9CC791B8BBB6E76AD&pf=qzone&serverid='
+		.($_GET['serverId']+1).'&pfkey=8b84f22eb855813b1c74011779e1aaa5');
